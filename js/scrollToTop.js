b676fabd4bdd6981672e5ad0625cda3a375ace4d
Eva Yan <eyan0749@gmail.com>
2022-05-19 20:12:17 -0400

    Add scroll to top button to homepage

---
+++ js/scrollToTop.js
@@ -0,0 +1,20 @@
+var mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollToTopButton()
+};
+
+function scrollToTopButton() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
